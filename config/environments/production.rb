3a7a79e3549086c81d613e611677762b499a5aa7
Josh Lippiner <jlippiner@noctivity.com>
2012-02-01 16:42:07 -0500

    added procfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,14 @@ Bday::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.action_mailer.default_url_options = { :host => "birthdaywishesforlisa.com" }
+
+  config.action_mailer.delivery_method :smtp
+  config.action_mailer.smtp_settings = {
+    enable_starttls_auto: true,
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: 'plain',
+    user_name: 'josh@birthdaywishesforlisa.com',
+    password: 'top$card'
+  }
 end
